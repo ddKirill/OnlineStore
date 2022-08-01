d345012c8d6073c4e9c96bfaf6634c4afa7f2bec
ddKirill <cfaleev@yandex.ru>
2022-08-02 00:24:40 +0300

    Add buttons

--- src/main/java/com/ddkirill/strore/telegrambot/StoreBot.java
+++ src/main/java/com/ddkirill/strore/telegrambot/StoreBot.java
@@ -12,6 +12,7 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.api.objects.media.InputMediaPhoto;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
@@ -27,6 +28,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static java.lang.Math.toIntExact;
+
 
 @Component
 public class StoreBot {
@@ -37,7 +40,6 @@ public class StoreBot {
     private final GetAllProducts getAllProducts;
     private final GetAllProductsWithFullInformation getAllProductsWithFullInformation;
     private final ReadTxt readTxt;
-
 
 
     public StoreBot(BotProperties botProperties, GetAllProducts getAllProducts, GetAllProductsWithFullInformation getAllProductsWithFullInformation, ReadTxt readTxt) throws TelegramApiException {
@@ -62,111 +64,123 @@ public class StoreBot {
             return botProperties.bot().token();
         }
 
-
         @Override
         public void onUpdateReceived(Update update) {
 
             if (update.hasMessage()) {
-                Message message = update.getMessage();
-                Chat chat = message.getChat();
-                User user = message.getFrom();
-
-                if (message.isCommand()) {
-
-                    if ("/start".equals(message.getText())) {
-                        //Send text by using ReadText service
-                        StringBuilder stringBuilder = new StringBuilder();
-                        String START_TEXT = "text/startText.txt";
-                        stringBuilder.append(readTxt.readTextFile(START_TEXT));
-                        sendTextMessage(chat.getId(), stringBuilder.toString());
-                        //Send Photo to chat
-                        SendPhoto sendPhoto = new SendPhoto();
-                        sendPhoto.setChatId(chat.getId().toString());
-                        sendPhoto.setPhoto(new InputFile(new File("text/Image1.jpg")));
+                    Message message = update.getMessage();
+                    Chat chat = message.getChat();
+                    User user = message.getFrom();
 
 
+                    //CommandHandlers
+                    if (message.isCommand()) {
+
+                        if ("/start".equals(message.getText())) {
+                            //Send text by using ReadText service
+                            StringBuilder stringBuilder = new StringBuilder();
+                            String START_TEXT = "text/startText.txt";
+                            stringBuilder.append(readTxt.readTextFile(START_TEXT));
+                            sendTextMessage(chat.getId(), stringBuilder.toString());
+                            //Send Photo to chat
+                            SendPhoto sendPhoto = new SendPhoto();
+                            sendPhoto.setChatId(chat.getId().toString());
+                            sendPhoto.setPhoto(new InputFile(new File("text/image.jpg")));
+
+                            sendPhoto.setReplyMarkup(new InlineKeyboardStart().getStartKeyboard());
+
+                            try {
+                                execute(sendPhoto);
+                            } catch (TelegramApiException e) {
+                                e.printStackTrace();
+                            }
+                        }
 
 
-                        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-                        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
-                        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
-                        inlineKeyboardButton1.setText("Information");
-                        inlineKeyboardButton1.setUrl("https://github.com/ddKirill/OnlineStore");
-                        inlineKeyboardButton2.setText("All products");
-                        inlineKeyboardButton2.setCallbackData("bla bla bla");
+                        if ("/allProducts".equals(message.getText())) {
+                            List<AllProducts> allProducts = getAllProducts.getAllProducts();
 
-                        List<InlineKeyboardButton> keyboardButtonList = new ArrayList<>();
-                        keyboardButtonList.add(inlineKeyboardButton1);
-                        keyboardButtonList.add(inlineKeyboardButton2);
+                            for (AllProducts allProduct : allProducts) {
+//
+                                String s = allProduct.getPrice() + "₽";
+                                String string = allProduct.getTitle() + "\n" + s;
+                                sendTextMessage(chat.getId(),string);
+                            }
 
-                        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
-                        rowList.add(keyboardButtonList);
+                        }
 
-                        inlineKeyboardMarkup.setKeyboard(rowList);
-                        sendPhoto.setReplyMarkup(inlineKeyboardMarkup);
+                        if ("/manageProducts".equals(message.getText())) {
+                            List<AllProductsWithFullInformation> allProductsList = getAllProductsWithFullInformation.getAllProducts();
 
-                        try {
-                            execute(sendPhoto);
-                        } catch (TelegramApiException e) {
-                            e.printStackTrace();
+                            for (AllProductsWithFullInformation products : allProductsList) {
+                                SendPhoto sendPhotoProduct = new SendPhoto();
+                                String price = products.getPrice() +"₽";
+                                String string = products.getTitle()
+                                        + "\n"
+                                        + price
+                                        + "\n"
+                                        + products.getDescription();
+                                sendPhotoProduct.setChatId(update.getMessage().getChatId().toString());
+                                sendPhotoProduct.setPhoto(new InputFile(new File(products.getLocationImage())));
+                                try {
+                                    execute(sendPhotoProduct);
+                                } catch (TelegramApiException e) {
+                                    e.printStackTrace();
+                                }
+                                sendTextMessage(chat.getId(),string);
+                            }
+
+                        }
+
+                        if ("/help".equals(message.getText())) {
+                            String HELP_TEXT = "text/helpText.txt";
+                            sendTextMessage(chat.getId(), readTxt.readTextFile(HELP_TEXT));
                         }
 
                     }
 
-
-
-                    if ("/allProducts".equals(message.getText())) {
-                        List<AllProducts> allProducts = getAllProducts.getAllProducts();
-                        StringBuilder stringBuilder = new StringBuilder();
-
-                        for (AllProducts allProduct : allProducts) {
-                            stringBuilder.append(allProduct.getTitle())
-                                    .append(" ")
-                                    .append(allProduct.getPrice())
-                                    .append("₽")
-                                    .append("\n");
-                        }
-                        sendTextMessage(chat.getId(), stringBuilder.toString());
+                    //NonCommandHandler
+                    else if (message.isUserMessage()) {
+                        sendTextMessage(chat.getId(), "Message is not a command\nPlease view Help menu /help");
                     }
+                }
+            if (update.hasCallbackQuery()) {
+                String callData = update.getCallbackQuery().getData();
+                long chatId = update.getCallbackQuery().getMessage().getChatId();
+                long messageId = update.getCallbackQuery().getMessage().getMessageId();
 
-                    if ("/manageProducts".equals(message.getText())) {
-                        List<AllProductsWithFullInformation> allProductsList = getAllProductsWithFullInformation.getAllProducts();
-                        StringBuilder stringBuilder = new StringBuilder();
-
-                        for (AllProductsWithFullInformation products : allProductsList) {
-                            stringBuilder.append(products.getTitle())
-                                    .append(" ")
-                                    .append(products.getPrice())
-                                    .append(" ")
-                                    .append(products.getDescription())
-                                    .append(" ")
-                                    .append(products.getLocationImage())
-                                    .append("\n");
-                        }
-                        sendTextMessage(chat.getId(), stringBuilder.toString());
+                if (callData.equals("news")) {
+                    String NEWS = "text/newsText.txt";
+                    EditMessageText editMessage = new EditMessageText();
+                    editMessage.setText("gwr,g,wv");
+                    editMessage.setChatId(String.valueOf(chatId));
+                    editMessage.setMessageId(toIntExact(messageId));
+                    try {
+                        execute(editMessage);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
                     }
                 }
             }
-
-
         }
 
-        //This method execute send message to user
-        private void sendTextMessage(Long chatId, String text) {
-            try {
+            //This method execute send message to user
+            private void sendTextMessage (Long chatId, String text){
+                try {
 
-                execute(
-                        SendMessage.builder()
-                                .text(text)
-                                .chatId(chatId.toString())
-                                .build()
+                    execute(
+                            SendMessage.builder()
+                                    .text(text)
+                                    .chatId(chatId.toString())
+                                    .build()
 
-                );
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+                    );
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
             }
+
         }
 
     }
 
-}
